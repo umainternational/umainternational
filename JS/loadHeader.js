b905f2302e7d64f4ed0e647e857839ec672612f7
umainternational <umainternational.travel@gmail.com>
2024-08-29 22:40:35 +0530

    Update loadHeader.js

--- JS/loadHeader.js
+++ JS/loadHeader.js
@@ -1,5 +1,5 @@
 function loadHeader() {
-    fetch('../umainternational/header.html')
+    fetch('https://umainternational.github.io/umainternational/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('headerContainer').innerHTML = data;
